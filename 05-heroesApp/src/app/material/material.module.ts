e9001883b510570349103c9ef02867c48d5979a1
kissmark <rivaskissmark@gmail.com>
2023-04-06 19:19:37 -0500

    add heroes

---
+++ 05-heroesApp/src/app/material/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ]
+})
+export class MaterialModule { }
